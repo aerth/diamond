1e39faf7e6aa042cb094efbf7119258df6b963d6
aerth <aerth@riseup.net>
2016-10-25 20:25:52 -0700

    httprouter example

---
+++ example/README.md
@@ -0,0 +1,5 @@
+This example shows usage with httprouter library
+
+Build, run, and visit http://127.0.0.1:8080/hello/world
+
+Make sure config.json exists
---
+++ example/httprouter.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+    "github.com/julienschmidt/httprouter"
+	"github.com/aerth/diamond/lib"
+    "net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func main() {
+    router := httprouter.New()
+    router.GET("/", Index)
+    router.GET("/hello/:name", Hello)
+
+	s := diamond.NewServer(router)
+	s.ConfigPath("config.json")
+	s.Start()
+	select {}
+}
