7cd94da7b123168cee47f72e4fdf06227f321dfa
aerth <aerth@riseup.net>
2017-03-31 23:14:25 -0700

    Grade A TLS/SSL

--- lib/003-levels.go
+++ lib/003-levels.go
@@ -23,9 +23,7 @@ var (
 	preferredCipherSuites = []uint16{
 		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-		tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
 		tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
-		tls.TLS_RSA_WITH_RC4_128_SHA,
 		tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
 		tls.TLS_RSA_WITH_AES_128_CBC_SHA,
 		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
@@ -33,6 +31,7 @@ var (
 		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 	}
 )
+
 func socketExists(path string) bool {
 	_, e := os.Open(path)
 	if e != nil {
