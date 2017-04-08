2536ed77f65adeb928b92530986c5950f7bfb2d6
aerth <aerth@riseup.net>
2017-04-08 14:33:23 -0700

    CGO_ENABLED off by default

--- build-admin.sh
+++ build-admin.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CGO_ENABLED=0 go build \
+CGO_ENABLED=${CGO_ENABLED:-0} go build \
     -o diamond-admin \
     -ldflags="-s -w -X 'main.clientname=Diamond Admin'" \
     github.com/aerth/diamond/cmd/diamond-admin
