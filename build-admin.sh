36c52c057f1734bc9d1814a8bacb8d724a3c1453
aerth <aerth@riseup.net>
2017-05-24 19:41:50 +0000

    remove old build script

--- build-admin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-CGO_ENABLED=${CGO_ENABLED:-0} go build \
-    -o diamond-admin \
-    -ldflags="-s -w -X 'main.clientname=Diamond Admin'" \
-    github.com/aerth/diamond/cmd/diamond-admin
